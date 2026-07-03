dea048248029e136051c3e625a830bcdaf759ea2
kvrp97 <ravinduprabhashwara97@gmail.com>
2026-07-04 00:12:08 +0530

    initial commit: setup

---
+++ src/test/java/lk/kvrp/creatorstore/CreatorStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.kvrp.creatorstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
