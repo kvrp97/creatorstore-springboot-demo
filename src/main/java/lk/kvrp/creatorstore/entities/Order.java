e3bd61713541da008e9a23aab565a3716c607d7c
kvrp97 <ravinduprabhashwara97@gmail.com>
2026-07-04 10:40:22 +0530

    Added entities for order

---
+++ src/main/java/lk/kvrp/creatorstore/entities/Order.java
@@ -0,0 +1,48 @@
+package lk.kvrp.creatorstore.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Customer's name is required")
+    @Column(name = "customer_name", nullable = false)
+    private String customerName;
+
+    @Column(name = "customer_email")
+    private String customerEmail;
+
+    @NotBlank(message = "Status is required")
+    @Column(nullable = false)
+    private String status;
+
+    @NotNull(message = "Total price is required")
+    @Column(name = "total_price", nullable = false)
+    private BigDecimal totalPrice;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems;
+
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+
+    public void prePersist() {
+        this.createdOn = LocalDateTime.now();
+    }
+}
