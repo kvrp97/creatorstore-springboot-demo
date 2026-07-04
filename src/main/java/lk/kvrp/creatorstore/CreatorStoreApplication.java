bec914ec08b00886a5ddd5afe7686a2b3e95c106
kvrp97 <ravinduprabhashwara97@gmail.com>
2026-07-04 06:49:55 +0530

    Added donenv config

--- src/main/java/lk/kvrp/creatorstore/CreatorStoreApplication.java
+++ src/main/java/lk/kvrp/creatorstore/CreatorStoreApplication.java
@@ -3,11 +3,22 @@ package lk.kvrp.creatorstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class CreatorStoreApplication {
 
 	public static void main(String[] args) {
+				
+		// Configuration of dotenv-java
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		dotenv.entries().forEach((entry) -> System.setProperty(
+			entry.getKey(), entry.getValue()
+		));
+
 		SpringApplication.run(CreatorStoreApplication.class, args);
+
 	}
 
 }
