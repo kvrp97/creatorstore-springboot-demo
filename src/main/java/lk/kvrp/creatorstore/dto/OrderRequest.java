11889ee6fecfbbc88c574ba91c4f3d2406bb3cf3
kvrp97 <ravinduprabhashwara97@gmail.com>
2026-07-05 09:59:48 +0530

    Updated: Organized imports.

--- src/main/java/lk/kvrp/creatorstore/dto/OrderRequest.java
+++ src/main/java/lk/kvrp/creatorstore/dto/OrderRequest.java
@@ -1,6 +1,5 @@
 package lk.kvrp.creatorstore.dto;
 
-import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
